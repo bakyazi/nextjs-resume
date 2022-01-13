c423368dc9cb8a1777cff3ab2b0cb5a462b98be5
bakyazi <berkayakyazi95@gmail.com>
2022-01-13 20:00:57 +0300

    title & redirect

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/global.css'
 import Header from '../components/header'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
     return (
         <div className="antialiased text-gray-700">
+            <Head>
+                <title>Berkay Akyazi</title>
+            </Head>
             <Header/>
             <main className="pt-24 mt-6 mb-20">
                 <Component {...pageProps} />
--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,24 +2,24 @@ import { NextResponse, NextRequest } from 'next/server'
 export async function middleware(req, ev) {
     const { pathname } = req.nextUrl
     if (pathname == '/blog') {
-        return NextResponse.redirect('https://blog.imberkay.com/')
+        return NextResponse.redirect('https://blog.imberkay.com/', 302)
     }
 
     if (pathname == '/twitter') {
-        return NextResponse.redirect('https://twitter.com/berkyzi')
+        return NextResponse.redirect('https://twitter.com/berkyzi', 302)
     }
 
     if (pathname == '/github') {
-        return NextResponse.redirect('https://github.com/bakyazi')
+        return NextResponse.redirect('https://github.com/bakyazi', 302)
     }
 
     if (pathname == '/linkedin') {
-        return NextResponse.redirect('https://www.linkedin.com/in/berkay-akyaz%C4%B1-a8b56310a/')
+        return NextResponse.redirect('https://www.linkedin.com/in/berkay-akyaz%C4%B1-a8b56310a/', 302)
     }
 
 
     if (pathname == '/instagram') {
-        return NextResponse.redirect('https://www.instagram.com/berkyzi/')
+        return NextResponse.redirect('https://www.instagram.com/berkyzi/', 302)
     }
     return NextResponse.next()
 }

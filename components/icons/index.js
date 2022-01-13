862000ee3aa5871736e0c9dad9f04b8128c169a1
bakyazi <berkayakyazi95@gmail.com>
2022-01-14 01:33:39 +0300

    new skills icon

---
+++ components/icons/index.js
@@ -0,0 +1,27 @@
+import GopherLogo from "./gopher";
+import PythonLogo from "./python";
+import JavaScriptLogo from "./javas";
+import SpringLogo from "./spring";
+import ReactLogo_ from "./react";
+import VueLogo from "./vue";
+import UbuntuLogo from "./ubuntu";
+import CentosLogo from "./centos";
+import JenkinsLogo from "./jenkins";
+import TravisLogo from "./travis";
+import JiraLogo from "./jira";
+import GithubLogo from "./github";
+import BitbucketLogo from "./bitbucket";
+
+export const Gopher = GopherLogo
+export const Python = PythonLogo
+export const JavaScript = JavaScriptLogo
+export const Spring = SpringLogo
+export const ReactLogo = ReactLogo_
+export const Vue = VueLogo
+export const Ubuntu = UbuntuLogo
+export const Centos = CentosLogo
+export const Jenkins = JenkinsLogo
+export const Travis = TravisLogo
+export const Jira = JiraLogo
+export const Github = GithubLogo
+export const Bitbucket = BitbucketLogo
--- pages/skills.js
+++ pages/skills.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactStars from "react-rating-stars-component";
 import { faJs, faJava, faPython, faReact, faVuejs, faGolang } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import GopherLogo from '../components/gopherlog';
+import { Gopher, Python, JavaScript, Spring, ReactLogo, Vue, Ubuntu, Centos, Jenkins, Travis, Jira, Bitbucket, Github } from '../components/icons';
 export function Skill({ icon, skill, rate }) {
     return (
         <>
@@ -17,13 +17,48 @@ export function Skill({ icon, skill, rate }) {
 export default function Skills() {
     return (
         <div className='site-container'>
-            <div className='skills-container'>
-                <div className='rate-card'><Skill skill="Java" rate={5} icon={<FontAwesomeIcon size="4x" icon={faJava} />} /></div>
-                <div className='rate-card'><Skill skill="Python" rate={5} icon={<FontAwesomeIcon size="4x" icon={faPython} />} /></div>
-                <div className='rate-card'><Skill skill="Golang" rate={4} icon={<GopherLogo />} /></div>
-                <div className='rate-card'><Skill skill="JavaScript" rate={4} icon={<FontAwesomeIcon size="4x" icon={faJs} />} /></div>
-                <div className='rate-card'><Skill skill="React" rate={4} icon={<FontAwesomeIcon size="4x" icon={faReact} />} /></div>
-                <div className='rate-card'><Skill skill="Vue" rate={3} icon={<FontAwesomeIcon size="4x" icon={faVuejs} />} /></div>
+            <div className='skills-sections '>
+                <div className='skills-section'>
+                    <label className='text-left text-cyan-700'>Programming Languages</label>
+                    <div className='skills-container'>
+                        <div className='rate-card'><Skill skill="Java" rate={5} icon={<FontAwesomeIcon size="4x" icon={faJava} />} /></div>
+                        <div className='rate-card'><Skill skill="Python" rate={5} icon={<Python />} /></div>
+                        <div className='rate-card'><Skill skill="Golang" rate={4} icon={<Gopher />} /></div>
+                        <div className='rate-card'><Skill skill="JavaScript" rate={4} icon={<JavaScript />} /></div>
+                    </div>
+                </div>
+                <div className='skills-section'>
+                    <label className='text-left text-cyan-700'>Frameworks</label>
+                    <div className='skills-container'>
+                        <div className='rate-card'><Skill skill="Spring" rate={5} icon={<Spring/>} /></div>
+                        <div className='rate-card'><Skill skill="React" rate={4} icon={<ReactLogo />} /></div>
+                        <div className='rate-card'><Skill skill="Vue" rate={3} icon={<Vue />} /></div>
+                    </div>
+                </div>
+                <div className='skills-section'>
+                    <label className='text-left text-cyan-700'>Operating Systems</label>
+                    <div className='skills-container'>
+                        <div className='rate-card'><Skill skill="Ubuntu" rate={5} icon={<Ubuntu/>} /></div>
+                        <div className='rate-card'><Skill skill="CentOS" rate={4} icon={<Centos />} /></div>
+                        <div className='rate-card'><Skill skill="CentOS" rate={4} icon={<Jenkins />} /></div>
+                    </div>
+                </div>
+                <div className='skills-section'>
+                    <label className='text-left text-cyan-700'>CI/CD</label>
+                    <div className='skills-container'>
+                        <div className='rate-card'><Skill skill="Jenkins" rate={4} icon={<Jenkins />} /></div>
+                        <div className='rate-card'><Skill skill="TravisCI" rate={2} icon={<Travis />} /></div>
+                    </div>
+                </div>
+                <div className='skills-section'>
+                    <label className='text-left text-cyan-700'>Tools</label>
+                    <div className='skills-container'>
+                        <div className='rate-card'><Skill skill="JIRA" rate={5} icon={<Jira />} /></div>
+                        <div className='rate-card'><Skill skill="Bitbucket" rate={5} icon={<Bitbucket />} /></div>
+                        <div className='rate-card'><Skill skill="GitHub" rate={5} icon={<Github />} /></div>
+                    </div>
+                </div>
+                {/* <div className='rate-card'><Skill skill="JavaScript" rate={4} icon={<FontAwesomeIcon size="4x" icon={faJs} />} /></div> */}
             </div>
         </div>
     )

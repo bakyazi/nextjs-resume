4115513f4ddf0ed4d9e899a474996dbd45d7b792
bakyazi <berkayakyazi95@gmail.com>
2022-01-13 22:26:57 +0300

    update aboutme text

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,14 @@ export default function HomePage() {
       <div className="site-container text-lg">
         <h2 className="text-2xl pb-4">Hi, I am <b className='text-cyan-700'>Berkay Akyazı</b></h2>
         <p className='pb-2'>
-          I am living in <b className='text-cyan-700 text-xl'>Ankara</b>, <b className='text-cyan-700 text-xl'>Turkey</b>. I am a full-stack developer. <br />
-          I am always looking for new challenges and opportunities. I am passionate about learning new technologies and I am always trying to improve myself.
+          I live in <b className='text-cyan-700 text-xl'>Ankara, Turkey</b>. I'm a full stack developer.  I am always looking for new challenges and opportunities. I am passionate about learning new technologies and are constantly trying to improve.
         </p>
         <p className='pb-2'>
-          I have been learning programming since I was a child. I started with <b className="text-cyan-700 text-xl">HTML</b>, <b className="text-cyan-700 text-xl">CSS</b> and a little bit <b className="text-cyan-700 text-xl">JavaScript</b> when I was at elementary school.
-          After I have started university, I have met with <b className="text-cyan-700 text-xl">Python</b> and <b className="text-cyan-700 text-xl">C/C++</b> programming languages, and my passion about programming has started. <br />
-          Later, I have met with <b className="text-cyan-700 text-xl" >Java</b> in my summer interviews. Since then, I have been writing Java in my profesional career.
+          I've been learning programming since I was a kid. When I was in elementary school, I started using <b className="text-cyan-700 text-xl">HTML, CSS,</b> and <b className="text-cyan-700 text-xl">JavaScript</b> a little. After I  started studying, I learned about the programming languages ​​<b className="text-cyan-700 text-xl">Python</b> and ​​<b className="text-cyan-700 text-xl">C / C++</b>, and my passion for programming began. After that, I  met  ​​<b className="text-cyan-700 text-xl">Java</b> in a summer internship. Since then, I've been writing Java in my professional career.
         </p>
         <p className='pb-2'>
-          In addition to Java, I am using Python, <b className="text-cyan-700 text-xl">Golang</b>, <b className="text-cyan-700 text-xl">React</b>, <b className="text-cyan-700 text-xl">Vue.js</b> in my profesional career and my daily life.
-          I adore Python for small practical projects, and web crawling. React is usually my first choice for frontend jobs. I am currently learning Golang and I am very passionate about it.
+          Besides Java, I use Python, <b className="text-cyan-700 text-xl">Golang, React</b> and <b className="text-cyan-700 text-xl">Vue.js</b> in my professional career and everyday life. I love Python for small hands-on projects and web crawls. React is usually my first choice for front-end work. I am currently studying Golang and am very passionate about it.
         </p>
-
       </div>
       <div className="site-3xl-container mt-20">
         <img src="/me.jpg" alt="sorry i can't find nice pic for here" />
